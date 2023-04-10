735e4dec6827264d3935157c455a67e4203ae13a
shnoopy23 <vika.akimova.1984@mail.ru>
2023-04-10 19:10:04 +0300

    Initial commit

---
+++ settings.py
@@ -0,0 +1,26 @@
+class Settings:
+    chrome_driver = "/tests/chromedriver.exe"
+
+    base_url = 'https://b2c.passport.rt.ru'
+    valid_email = 'akimova@yandex.ru'       # Ввести валидный email
+    valid_password = '@@********'   # Ввести валидный пароль
+    valid_email_reg = 'Ввести валидный email'   # Ввести валидный email
+    invalid_email = 'shnop.alid.com'
+    invalid_password = 'invalid123456'
+    empty_email = ''
+    empty_password = ''
+    menu_type_auth = ['Телефон', 'Почта', 'Логин', 'Лицевой счёт']
+    placeholder_name = ['Мобильный телефон', 'Электронная почта', 'Логин', 'Лицевой счёт']
+    first_name = 'Виктория'
+    last_name = 'Викторова'
+    first_name_ = '-'
+    last_name_ = '-'
+    russian_generate_string = 'ф'
+    latin_generate_string = 'invalid'
+    chinese_chars = '形聲字象形字形声字/'
+    special_chars = '()_+=/{}!@#$%^&*'
+    numbers = 555
+    empty = ''
+    passw1 = 'QwE12345'
+    passw2 = 'Z56vBn09Dswa9874'
+    passw3 = 'W321Ujb30aSwsX1987654'
